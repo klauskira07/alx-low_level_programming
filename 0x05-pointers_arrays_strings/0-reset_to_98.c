4414d25a195002e98406b7aeb6302d3188ced6af
Klaus KIRA <klauskira07@gmail.com>
2022-03-22 17:30:11 -0600

    98 Battery st

---
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,13 @@
+#include 'main.h"[D[D[D[D[D[D["[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
+
+[1;2o[1;2j[1;2j/**
+ *reset_to_98 - updates a integre value to 98
+ *0n: A pointer to an int that will be updted
+ *
+ *Return: void that means is correct
+ */
+
+void reset_to_98(int *n);
+{
+	*n = 98;
+}
---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,11 @@
+int _putchar(char c);
+void swap_int(int *a, int *b);
+int_strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *S);[D[s[C[C
+void puts2(char *str);
+void puts[B_half(char *str);
+void print_array(int *a, int *n);[D[D[D[D[D[D[D[D[D[[3~[3~[3~[3~[C[C[[C[C[C[C[C[C[C[C[C[C[C[C[C[C *a, int n);
+char *_strcpy(char *dest, char *src);
+int _atoi(char *s);
