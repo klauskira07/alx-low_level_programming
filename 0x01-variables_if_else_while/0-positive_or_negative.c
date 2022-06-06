ddb53d075c590927a263585c2e69dcbb36cae276
Klaus KIRA <klauskira07@gmail.com>
2022-06-06 13:34:48 -0600

    Negative and Positive

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -2,19 +2,22 @@
 #include <time.h>
 #include <stdio.h>
 
-/* Documentation - main : Exercise that lights up negative numbers from positives ones*/
-	int main(void)
-	{
-		int n;
+/* 
+   main - Program that shows up whether a number is positive or negative
+   Return : 0
+*/
+int main(void)
+{
+    int n;
 
-		srand(time(0));
-		n = rand() - RAND_MAX / 2;
-		/*  */
-	 	if (n > 0)
-        		printf("%d is positive\n", n);
-   		else if (n < 0)
-        		printf("%d is negative\n", n);
-    		else
-         		printf("%d is zero\n", n);
-		return (0);
-	}
+    srand(time(0));
+    n = rand() - RAND_MAX / 2;
+    /*  */
+    if (n > 0)
+            printf("%d is positive\n", n);
+    else if (n < 0)
+            printf("%d is negative\n", n);
+    else
+            printf("%d is zero\n", n);
+    return (0);
+}
