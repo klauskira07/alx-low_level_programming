9a6cd4b9f90258587db111afc7da95adacb259b5
Klaus KIRA <klauskira07@gmail.com>
2022-06-13 10:32:08 -0600

    Battery St

--- 0x05-pointers_arrays_strings/0-reset_to_98.c
+++ 0x05-pointers_arrays_strings/0-reset_to_98.c
@@ -1,6 +1,6 @@
-#include 'main.h"[D[D[D[D[D[D["[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[C[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D
+#include "main.h"
 
-[1;2o[1;2j[1;2j/**
+/**
  *reset_to_98 - updates a integre value to 98
  *0n: A pointer to an int that will be updted
  *
