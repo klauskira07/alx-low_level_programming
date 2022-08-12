c701476760e2a7f6d7ce9c94e23a290fa83cce23
Klaus KIRA <92312934+klauskira07@users.noreply.github.com>
2022-08-13 00:25:42 +0200

    Update header file

--- 0x13-more_singly_linked_lists/lists.h
+++ 0x13-more_singly_linked_lists/lists.h
@@ -1,3 +1,29 @@
+
+#ifndef LISTS_H
+#define LISTS_H
+
+/*
+ * File: lists.h
+ * Desc: Header file containing prototypes and definitions for all functions
+ *       and types written in the 0x12-more_singly_linked_lists directory.
+ */
+
+#include <stdlib.h>
+
+/**
+ * struct listint_s - singly linked list
+ * @n: integer
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ * for ALX project
+ */
+typedef struct listint_s
+{
+	int n;
+	struct listint_s *next;
+} listint_t;
+
 int _putchar(char a);
 size_t print_listint(const listint_t *h);
 size_t listint_len(const listint_t *h);
